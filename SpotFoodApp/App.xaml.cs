2be934ab9890ac1ed784b28d2c844a0a82914a75
zawy1130 <giahuy34488@gmail.com>
2026-03-17 18:07:15 +0700

    Create project

---
+++ SpotFoodApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SpotFoodApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "SpotFoodApp" };
+        }
+    }
+}
---
+++ SpotFoodApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SpotFoodApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
